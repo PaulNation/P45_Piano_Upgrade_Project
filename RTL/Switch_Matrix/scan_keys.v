9b256e629151f6b63bb3754780e781889811520a
Paul Nieves <pnieves1234@gmail.com>
2026-07-16 20:17:00 -0400

    New Feature: added row and scan sequence to scan keys module to have stable sampling

--- RTL/Switch_Matrix/scan_keys.v
+++ RTL/Switch_Matrix/scan_keys.v
@@ -7,6 +7,7 @@ module scan_keys (
     output reg [14:0] row_n,  //A row is driven to low with a Push Pull IO for scanning the columns of that row, when a key is pressed, the corresponding column will be pulled low
 );
     reg [6:0] current_col; // Current column being scanned
+    reg [1:0] hold_row; // 00 hold the current row for a clock cycle without sampling, 01 samples the columns for the current row, 10 moves to the next row
     // State of the keys (pressed or not pressed)
     reg [1:0] key_state [0:6]; // key_state[i] = 2'b00: switches not pressed, 2'b01: m1 switch pressed, 2'b10: m2 switch pressed, 2'b11: both switches pressed
     
@@ -14,32 +15,29 @@ module scan_keys (
         if (!rst_n) begin
             row_n <= 15'b111111111111111; // All rows inactive (high)
             key_state <= '{default: 2'b00}; // Reset all key states to not pressed
-        end else begin
-            // Logic to scan the switch matrix and update row_n based on col_m1_n and col_m2_n
-            // This is a placeholder for the actual scanning logic
-            if (row_n == 15'b111111111111111) begin
-                row_n <= 15'b011111111111111; // Activate the first row
-            end else begin
-                row_n <= {1'b1, row_n[14:1]}; // Shift the active row to the next one
-                for (int i = 0; i < 6; i++) begin
-                    key_state[current_col+i] <= {col_m1_n[i], col_m2_n[i]}; // Update key state based on the current column values
-                end
-                // Key and the state of its two switches is captured in key_state[current_col+i] for the current column being scanned
-            end
-        end
-    end
-
-    always @(posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
-            // Reset logic for any internal state if needed
+            hold_row <= 2'b00; // Reset hold_row for the first row
             current_col <= 7'h00; // Reset to the first column
         end else begin
-            // Additional logic for handling key presses, debouncing, etc.
-            if (current_col == 7'h53) begin
-                current_col <= 7'h00; // Wrap around to the first column after the last column
-            end
-            else begin
-                current_col <= current_col + 6; // Move to the next column
+            if (row_n == 15'b111111111111111) begin
+                row_n <= 15'b011111111111111; // Activate the first row
+                hold_row <= 2'b00; // Reset hold_row
+                current_col <= 7'h00; // Reset to the first column after the last row has been scanned
+            end else begin
+                if (hold_row == 2'b01) begin //Sampling State: Sample the columns for the current row
+                    hold_row <= hold_row + 1; // Increment hold_row to maintain the current row for stability
+                    current_col <= current_col + 6; // Move to the next column
+                    for (int i = 0; i < 6; i++) begin
+                        key_state[current_col+i] <= {~col_m1_n[i], ~col_m2_n[i]}; // Update key state based on the current column values
+                    end
+                end
+                else if (hold_row == 2'b10) begin // Next Row State: Move to the next row
+                    row_n <= {1'b1, row_n[14:1]}; // Shift the active row to the next one
+                    // Key and the state of its two switches is captured in key_state[current_col+i] for the current column being scanned
+                    hold_row <= 2'b00; // Reset hold_row for the first row
+                end 
+                else begin // Hold State: Maintain the current row for stability
+                    hold_row <= hold_row + 1; // Reset hold_row when moving to the next row
+                end
             end
         end
     end
